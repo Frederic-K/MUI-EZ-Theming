81db7a4c1fecb4d43f5a96d8deac39844cf056f6
frederic.k <frederic.kreuder.pro@gmail.com>
2023-10-18 17:50:37 +0200

    add new themes

--- src/themes/Themes.jsx
+++ src/themes/Themes.jsx
@@ -1,52 +1,73 @@
 import { useMemo } from 'react'
 import { createTheme } from '@mui/material/styles'
 import { teal, purple, amber, deepOrange, grey } from '@mui/material/colors'
+import darkBG from '../assets/5053309-dark.jpg'
+import lightBG from '../assets/5053309-light.jpg'
 
 export default function Themes(modeTheme) {
   const mode = modeTheme
 
-  // const violetTheme = createTheme({
-  //   palette: {
-  //     mode: 'light',
-  //     primary: {
-  //       main: '#8471c9',
-  //     },
-  //     secondary: {
-  //       main: '#f50057',
-  //     },
-  //   },
-  // })
+  const violetTheme = createTheme({
+    palette: {
+      // mode: 'light',
+      primary: {
+        main: '#8471c9',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+    },
+  })
+
+  const purpleTheme = createTheme({
+    palette: {
+      // mode: 'light',
+      mode,
+      primary: {
+        ...purple,
+        ...(mode === 'light' && {
+          main: purple[900],
+        }),
+        ...(mode === 'dark' && {
+          main: purple[300],
+        }),
+      },
+      secondary: {
+        main: '#f50057',
+      },
+    },
+  })
 
   // Default dark mode
-  // const defaultTheme = useMemo(
-  //   () =>
-  //     createTheme({
-  //       palette: {
-  //         mode,
-  //       },
-  //     }),
-  //   [mode]
-  // )
+  const defaultTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode]
+  )
 
   // Upgrade default dark mode
-  // const StPatrickTheme = useMemo(
-  //   () =>
-  //     createTheme({
-  //       palette: {
-  //         mode,
-  //         primary: {
-  //           ...teal,
-  //           ...(mode === 'dark' && {
-  //             main: teal[300],
-  //           }),
-  //         },
-  //         secondary: {
-  //           main: '#f5f5f5',
-  //         },
-  //       },
-  //     }),
-  //   [mode]
-  // )
+  const StPatrickTheme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+          primary: {
+            ...teal,
+            ...(mode === 'dark' && {
+              main: teal[300],
+            }),
+          },
+          secondary: {
+            main: '#f5f5f5',
+          },
+        },
+      }),
+    [mode]
+  )
 
   const halloweenTheme = useMemo(
     () =>
@@ -81,6 +102,6 @@ export default function Themes(modeTheme) {
     [mode]
   )
   return {
-    theme: halloweenTheme,
+    theme: purpleTheme,
   }
 }
